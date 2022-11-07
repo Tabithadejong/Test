220a1e1a3c0c66fcadeee984c756e558ec82bf73
Tabithadejong <tabithadejong654@gmail.com>
2022-11-07 21:07:59 +0000

    Still trying different ways to design the game

--- run.py
+++ run.py
@@ -3,23 +3,36 @@ score =[]
 print("Question 1:")
 while True:
     try:
-        
-        answer1 = float(input("Enter your first number:\n"))
+        answer1 = int(input("Enter your first number:\n"))
     except ValueError:
         print("\nWhat you have entered is not a valid number, try again.")
     else:
-        score.append(answer1)
+        if answer1 <= 10:
+            score.append(answer1)
+        else: 
+            raise ValueError("Input needs to be under 10")   
         break 
 print("Question 2:")
 while True:
     try:
-        
         answer2 = float(input("Enter your first number:\n"))
     except ValueError:
         print("\nWhat you have entered is not a valid number, try again.")
     else:
         score.append(answer2)
         break 
+
+print("Question3") 
+
+while True:
+    answer3 = int(input("type here:"))
+    if answer3 >= 11 : 
+        raise ValueError("number must be under 10:")
+    else: 
+        print("well done:")
+        
+
+score.append(answer3)
 
 total = sum(score)
 print(total)
@@ -35,18 +48,31 @@ def result(number):
     else: 
         print("maybe baby")
 
-    """
-    if int(number) >= 1 and int(number) >= 10: 
-        print("cold")
-    elif int(number) >= 10 and int(number) >= 20: 
-        print("warrior")
-    else: 
-        print("hottie")
-
-    """
 
 person = result(total)
 print(result)
+
+
+def ask_for_contact(): 
+    """ 
+    Ask for personal contact details, 
+    and push them to a sheet 
+    """
+
+    print("would you like to recieve news on angermanagement training? leave us your details.")
+    input("yes/no")
+    name = input("Your Name is: ")
+    phone= input("Your phone number is:")
+    mail = input("email is:")
+    print(f"Thank you {name} we will ring you soon at {phone}")
+
+ask_for_contact()
+
+
+
+
+
+
     
     
 
