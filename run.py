263742bcd6157c399c3fc3e72f7cd0eb65e7909c
Tabithadejong <tabithadejong654@gmail.com>
2022-11-07 19:43:59 +0000

    Add function to calculate score

--- run.py
+++ run.py
@@ -3,6 +3,7 @@ score =[]
 print("Question 1:")
 while True:
     try:
+        
         answer1 = float(input("Enter your first number:\n"))
     except ValueError:
         print("\nWhat you have entered is not a valid number, try again.")
@@ -12,6 +13,7 @@ while True:
 print("Question 2:")
 while True:
     try:
+        
         answer2 = float(input("Enter your first number:\n"))
     except ValueError:
         print("\nWhat you have entered is not a valid number, try again.")
@@ -19,5 +21,19 @@ while True:
         score.append(answer2)
         break 
 
-print(score)   
+total = sum(score)
+print(total)
+
+def result(value): 
+    if int(value) == range(1, 3): 
+        print("cold")
+    elif int(value) == range(3, 6): 
+        print("warrior")
+    else: 
+        print("hottie")
+
+# person = result(total)
+# print(result)
+    
+    
 
