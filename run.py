366b85c1711851e15eaf0e32df931a5242499eb3
Tabithadejong <tabithadejong654@gmail.com>
2022-11-11 15:45:19 +0000

    Add real text for questions and results instead of place holder text

--- run.py
+++ run.py
@@ -5,12 +5,12 @@ def introduction():
     ask if the person would like to start the game
     """ 
 
-    name = input("Enter your name: ")
-    print(f" Hi there {name} welcome to our anger-personality-test\n")
+    name = input("Enter your name:\n ")
+    print(f"Hi there {name} welcome to our anger-personality-test\n")
     print("You will be asked a set of questions and your answer will need to be a number on the scale of 10")
     print("Make sure not to type in a number higher than 10 or your results will be inaccurate")
     print("If at any time you would like to start over just click the Run Program button above the terminal ")
-    begin()
+    return name
 
 
 def begin():
@@ -24,9 +24,9 @@ def begin():
     
     if start == "y": 
         print("Great well let's go\n")
-        total_score= quizz()
     elif start == "n" : 
         print(" Thats too bad, if you cange your mind let us know!")
+        main()
     else: 
         print("Oh Oh, that is not a valid input")
         begin()
@@ -36,11 +36,18 @@ def begin():
 def quizz(): 
     """ 
     The function that holds all questions, 
-    plus builds a list for score. 
+    also builds a list for score. 
+    The loop will display the questions and answer input
     """
     score = []
     questions = ("When your friend annoys you, do you tell him/her?:",
-     " Q2", " Q3", " Q4", " Q5",)
+     "When the waiter is rude, do you complain?", 
+     "When the food comes out different than you ordered, will you send it back? ",
+      "Do you get frustrated while driving?",
+       "Would you tell the truth even when it will hurt? ",
+       "When your employer tells you to be quiet, would you listen?",
+       "When a customer is behaving like a spoiled brat, would you point this out? ",
+       "Do you have arguments with your peers often? ")
 
     for q in questions: 
         value = get_answer(q)
@@ -57,8 +64,8 @@ def get_answer(quest):
     if not raise a type error and if correct,
     returns the number
     """
-    print(quest) 
-    print("Choose a number on a scale of 1 to 10.\n")
+    print(quest + " \n ") 
+    print("Choose a number on a scale of 0 to 10.\n")
 
     
     while True:
@@ -74,29 +81,34 @@ def get_answer(quest):
 
 
 def result(number): 
+
     """ 
-    Calculate the total score, 
+    Matches the score with right type, 
     print right anger-type
     """
-    cool = "The coolest"
-    warrior = "Neutral"
-    hothead = "angry"
+    cool = "Wow it seems like you are as cool as icecream. Even though it is great to be in control nobody likes to relate to a statue/n Also your anger is the force for change, don't let it die!"
+    warrior = "You seem to have a warrior like state of mind. You know when to loose your cool to address that what needs change.\n But you also know when to maintain your cool."
+    hothead = "You are overheated most of the time, we could call you dynamite. Bring back the 10 sec count before expressing your anger!"
+    off-the-chart = "Ooops, your score seems to be off the charts. Did you type in numbers higher than 10? Let's start all the way from the top!"
+    print("\nCalculating your angertype.....\n")
 
-    print("Calculating your angertype.....")
-
-    if int(number) <= 10: 
+    if int(number) <= 24: 
         return cool
-    elif int(number) <= 17: 
+    elif int(number) <= 56: 
         return warrior
-    else: 
+    elif int(number) <= 81:
         return hothead
+    else: 
+        return off-the-chart 
 
 def main(): 
     """ 
     Calls all the main functions for the game
     """
 
-    introduction()
+    user = introduction()
+    begin()
+    total_score= quizz()
     person = result(total_score)
     print(person)
 
