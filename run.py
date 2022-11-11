6d1363f2b4cf7d99900d13e9444bb898f01752d6
Tabithadejong <tabithadejong654@gmail.com>
2022-11-11 17:29:54 +0000

    Rewrite the get_answer function to check the input to be < 10

--- run.py
+++ run.py
@@ -6,7 +6,7 @@ def introduction():
     """ 
 
     name = input("Enter your name:\n ")
-    print(f"Hi there {name} welcome to our anger-personality-test\n")
+    print(f"Hi there, {name} welcome to our anger-personality-test\n")
     print("You will be asked a set of questions and your answer will need to be a number on the scale of 10")
     print("Make sure not to type in a number higher than 10 or your results will be inaccurate")
     print("If at any time you would like to start over just click the Run Program button above the terminal ")
@@ -25,7 +25,7 @@ def begin():
     if start == "y": 
         print("Great well let's go\n")
     elif start == "n" : 
-        print(" Thats too bad, if you cange your mind let us know!")
+        print("Thats too bad, if you cange your mind let us know!")
         main()
     else: 
         print("Oh Oh, that is not a valid input")
@@ -61,22 +61,22 @@ def get_answer(quest):
     """ 
     Prints the question.
     Checks the input to be a number, 
-    if not raise a type error and if correct,
+    if the num is between 1 -10.
+    If not raise an error and if correct,
     returns the number
     """
-    print(quest + " \n ") 
-    print("Choose a number on a scale of 0 to 10.\n")
+    print(" \n" + quest + " \n ") 
+    print("Choose a number on a scale of 1 to 10.")
 
-    
+    # thank god to stack overflow
+
     while True:
-        try:
-            answer = int(input("1 = Never and 10 = Always:\n"))
-        except ValueError:
-            print("\nWhat you have entered is not a valid number.\n ")  
-            print("Make sure your number is between 1 and 10 otherwise your results will be off the charts.")
-            print("Try again.")
-        else: 
-            return answer 
+        answer = input("1 = Never 10 = Always:\n")
+        if answer.isdigit() and 1 <= int(answer) <= 10:
+            return int(answer)
+            break
+        else:
+            print("This is not a valid input")
       
 
 
@@ -86,10 +86,12 @@ def result(number):
     Matches the score with right type, 
     print right anger-type
     """
-    cool = "Wow it seems like you are as cool as icecream. Even though it is great to be in control nobody likes to relate to a statue/n Also your anger is the force for change, don't let it die!"
-    warrior = "You seem to have a warrior like state of mind. You know when to loose your cool to address that what needs change.\n But you also know when to maintain your cool."
-    hothead = "You are overheated most of the time, we could call you dynamite. Bring back the 10 sec count before expressing your anger!"
-    off-the-chart = "Ooops, your score seems to be off the charts. Did you type in numbers higher than 10? Let's start all the way from the top!"
+
+    cool = "Wow it seems like you are as cool as icecream. Even though it is great to be in control nobody likes to relate to a statue\nAlso your anger is the force for change, don't let it die!"
+    warrior = "You seem to have a warrior like state of mind. You know when to lose your cool to address, that what needs change.\nBut you also know when to control your emotion to adjust to the situation."
+    hothead = "You are overheated most of the time, we could call you dynamite. Bring back the 10 second count before expressing your anger!"
+    off_chart = "Ooops, your score seems to be off the charts. Did you type in numbers higher than 10?\nPress Run Game above the terminal and let's start all the way from the top!"
+
     print("\nCalculating your angertype.....\n")
 
     if int(number) <= 24: 
@@ -99,7 +101,7 @@ def result(number):
     elif int(number) <= 81:
         return hothead
     else: 
-        return off-the-chart 
+        return off_chart
 
 def main(): 
     """ 
@@ -108,11 +110,14 @@ def main():
 
     user = introduction()
     begin()
-    total_score= quizz()
+    total_score = quizz()
     person = result(total_score)
     print(person)
+    
 
 main()
+
+
 
 
 
