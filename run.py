ce5564562c4c0f8ac4b2caf81200f7077f8ff3e9
Tabithadejong <tabithadejong654@gmail.com>
2022-11-11 14:14:20 +0000

    Build a tuple for questions, than write a for loop to run through questions

--- run.py
+++ run.py
@@ -2,33 +2,39 @@
 def quizz(): 
     """ 
     The function that holds all questions, 
-    keeps track of score
+    plus builds a list for score. 
     """
     score = []
+    questions = ("Q1", " Q2", " Q3", " Q4", " Q5",)
 
-    print("When your partner would lie to you would you be angry?:")
-    while True:
-        try:
-            answer1 = int(input("Put in a number between 1=Never and 10=Always:\n"))
-        except ValueError:
-            print("\nWhat you have entered is not a valid number, try again.")
-        else:
-            score.append(answer1) 
-            break 
-    print("Question 2:")
-    while True:
-        try:
-            answer2 = float(input("Put in a number between 1=Never and 10=Always:\n"))
-        except ValueError:
-            print("\nWhat you have entered is not a valid number, try again.")
-        else:
-            score.append(answer2)
-            break 
+    for q in questions: 
+        value = get_answer(q)
+        score.append(value) 
 
     total = sum(score)
-    print(total) 
     return total 
-        
+
+def get_answer(quest): 
+
+    """ 
+    Checks the input to be a number, 
+    if not raise a type error and if correct,
+    returns the number
+    """
+    print(quest) 
+    print("Choose a number on a scale of 1 to 10.\n")
+
+    
+    while True:
+        try:
+            answer = int(input("1 = Never and 10 = Always:\n"))
+        except ValueError:
+            print("\nWhat you have entered is not a valid number.\n ")  
+            print("Make sure your number is between 1 and 10 otherwise your results will be off the charts.")
+            print("Try again.")
+        else: 
+            return answer 
+      
 
 
 def result(number): 
@@ -40,7 +46,7 @@ def result(number):
     warrior = "Neutral"
     hothead = "angry"
 
-   
+    print("Calculating your angertype.....")
 
     if int(number) <= 10: 
         return cool
@@ -49,10 +55,17 @@ def result(number):
     else: 
         return hothead
 
+def main(): 
+    """ 
+    Calls all the main functions for the game
+    """
 
-total_score= quizz()
-person = result(total_score)
-print(person)
+    total_score= quizz()
+    person = result(total_score)
+    print(person)
+
+main()
+
 
 
 
